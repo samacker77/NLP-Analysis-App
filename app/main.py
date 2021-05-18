66566ddb41105c48059b9b432edde9e942eea609
Samar Srivastava <samarsrivastava44@gmail.com>
2021-05-18 23:40:24 +0530

    Added more stuff

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/<page>')
+def gotoNextPage(page):
+    return render_template(str(page)+'.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+

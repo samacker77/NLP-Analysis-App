dd4d9f16c97eaff5645db89d975eab4dbdd4f373
nitika-kamboj <nitika@greendeck.co>
2021-05-20 21:37:10 +0530

    first functionality complete

--- app/main.py
+++ app/main.py
@@ -1,26 +1,54 @@
-from flask import Flask, flash, request, redirect, url_for,render_template
+from flask import Flask, flash, request, redirect, url_for,render_template,send_file
 from werkzeug.utils import secure_filename
 import os
+import shutil
+
 UPLOAD_FOLDER = 'app/uploads/'
+DOWLOAD_FOLDER= 'app/downloads/'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'doc','docx'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWLOAD_FOLDER
 app.secret_key = "jhkafshjkfhfsd"
 
 
 @app.route('/',methods=['POST','GET'])
 def home():
-
-    if request.method == 'POST':
+    try:
+        uploaded_files = os.listdir(UPLOAD_FOLDER)
+    except:
+        uploaded_files = ['No uploaded files']
+    if len(uploaded_files)==0:
+        uploaded_files=['No uploaded files']
+    if request.method == 'POST' and request.form.get('upload'):
         file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = file.filename
+        if request.form['filename']!="":
+            filename=request.form['filename']
+        else:
+            filename=file.filename
+        if file and allowed_file(filename):
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             flash("File Uploaded")
-    return render_template('index.html')
+            uploaded_files = os.listdir(UPLOAD_FOLDER)
+        else:
+            flash("File cannot be uploaded")
+    if request.method == 'POST' and request.form.get('download'):
+        if request.form['downloadfilename'] !="":
+            filename=request.form['downloadfilename']
+            if filename in uploaded_files:
+                path = 'uploads/'+ filename
+                return send_file(path, as_attachment=True)
+            else:
+                flash("No such file exists")
+        else:
+            flash("Please mention filename")
+
+    return render_template('index.html',uploaded_files=uploaded_files)
+
 @app.route('/<page>')
 def gotoNextPage(page):
+    print(page)
     return render_template(str(page)+'.html')
 
 
